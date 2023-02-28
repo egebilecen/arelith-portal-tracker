a6333cce9714afdd65b6b8392f0adea964eaa88a
Ege Bilecen <egebilecen@icloud.com>
2023-02-28 09:27:02 +0300

    added localhost default path value

--- dashboard/inc/core.php
+++ dashboard/inc/core.php
@@ -1,6 +1,9 @@
 <?php
 
-define("MAIN_PATH", "/home/username/");
+if (in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']))
+    define("MAIN_PATH", "C:\\wamp64\\www\\arelith-portal-tracker\\");
+else
+    define("MAIN_PATH", "/home/username/");
 
 require_once(MAIN_PATH . "config.php");
 require_once(MAIN_PATH . "portal_util.php");
